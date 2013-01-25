12dd24a0e1a522b76834fb99d62ade7934137b27
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-01-25 23:44:04 +0100

    Minor cleanup of configuration.

--- config/Externals.cmake
+++ config/Externals.cmake
@@ -1,13 +1,26 @@
 include(ExternalProject)
 
-include(BoostMinimal)
-include(GTest-1.6)
-include(Hiredis)
-include(Redis)
-include(JavaScriptCore)
-include(JSObjects)
-include(SwigJS)
+FIND_PACKAGE(Boost)
+if(Boost-NOTFOUND)
+  include(BoostMinimal)
+endif()
+
+if(NOT EXISTS ${SWIG_COMMAND})
+  message("###### NOTE: you can avoid downloading and building the swig-v8 project by providing a CMake variable 'SWIG_COMMAND'")
+  include(SwigJS)
+endif()
+
+if(ENABLE_TESTING)
+  include(GTest-1.6)
+endif()
+
+if(ENABLE_JSC)
+  include(JavaScriptCore)
+endif()
 
 if (ENABLE_V8)
   include(V8)
 endif()
+
+include(JSObjects)
+include(Hiredis)
