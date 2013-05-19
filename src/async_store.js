2af7e07a81f741d569363f18f6bac7913532915b
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-05-20 01:44:37 +0200

    Cleanup.

--- src/async_store.js
+++ src/async_store.js
@@ -16,23 +16,24 @@
   }
 
   var AsyncStore = function(store) {
+    this.store = store;
+  };
 
-    var proto = util.prototype(this);
-    var self = this;
+  AsyncStore.__prototype__ = function() {
 
-    proto.exists = function (id, cb) {
-      var result = store.exists(id);
+    this.exists = function (id, cb) {
+      var result = this.store.exists(id);
       cb(null, result);
     };
 
-    proto.create = function (id, options, cb) {
+    this.create = function (id, options, cb) {
       if (arguments.length == 2 && _.isFunction(options)) {
         cb = options;
         options = null;
       }
       var result;
       try {
-        result = store.create(id, options);
+        result = this.store.create(id, options);
       } catch (err) {
         return cb(err);
       }
@@ -42,56 +43,56 @@
     // Get document info (no contents)
     // --------
 
-    proto.getInfo = function(id, cb) {
-      var result = store.getInfo();
+    this.getInfo = function(id, cb) {
+      var result = this.store.getInfo(id);
       if(!result) return cb(new errors.StoreError("Document does not exist."));
       cb(null, result);
     };
 
-    proto.list = function (cb) {
-      var result = store.list();
+    this.list = function (cb) {
+      var result = this.store.list();
       cb(null, result);
     };
 
-    proto.get = function(id, cb) {
-      var result = store.get(id);
+    this.get = function(id, cb) {
+      var result = this.store.get(id);
       if(!result) return cb(new errors.StoreError("Document does not exist."));
       cb(null, result);
     };
 
-    proto.commits = function(id, last, since, cb) {
-      var result = store.commits(id, last, since);
+    this.commits = function(id, last, since, cb) {
+      var result = this.store.commits(id, last, since);
       cb(null, result);
     };
 
-    proto.delete = function (id, cb) {
-      store.delete(id);
+    this.delete = function (id, cb) {
+      this.store.delete(id);
       cb(null);
     };
 
-    proto.clear = function(cb) {
-      store.clear();
+    this.clear = function(cb) {
+      this.store.clear();
       cb(null);
     };
 
-    proto.update = function(id, options, cb) {
+    this.update = function(id, options, cb) {
       if (arguments.length == 2) {
         cb = options;
         options = null;
       }
       try {
-        store.update(id, options);
+        this.store.update(id, options);
       } catch (err) {
         return cb(err);
       }
       cb(null);
     }
 
-    proto.getRefs = function(id, branch, cb) {
+    this.getRefs = function(id, branch, cb) {
 
       if (arguments.length == 2 && _.isFunction(branch)) {
         cb = branch;
-        var refs = store.getRefs(id);
+        var refs = this.store.getRefs(id);
         return cb(null, refs);
       }
 
@@ -99,63 +100,64 @@
       cb(null, refs);
     };
 
-    proto.setRefs = function(id, branch, refs, cb) {
-      store.setRefs(id, branch, refs);
+    this.setRefs = function(id, branch, refs, cb) {
+      this.store.setRefs(id, branch, refs);
       cb(null);
     };
 
-    proto.deletedDocuments = function(cb) {
-      var result = store.deletedDocuments();
+    this.deletedDocuments = function(cb) {
+      var result = this.store.deletedDocuments();
       cb(null, result);
     };
 
-    proto.confirmDeletion = function(id, cb) {
-      store.confirmDeletion(id);
+    this.confirmDeletion = function(id, cb) {
+      this.store.confirmDeletion(id);
       cb(null);
     };
 
-    proto.seed = function(data, cb) {
-      store.seed(data);
+    this.seed = function(data, cb) {
+      this.store.seed(data);
       cb(null);
     };
 
-    proto.dump = function(cb) {
-      var result = store.dump();
+    this.dump = function(cb) {
+      var result = this.store.dump();
       cb(null, result);
     };
 
-    proto.createBlob = function(docId, blobId, base64data, cb) {
+    this.createBlob = function(docId, blobId, base64data, cb) {
       var result;
       try {
-        result = store.createBlob(docId, blobId, base64data);
+        result = this.store.createBlob(docId, blobId, base64data);
       } catch(err) {
         return cb(err);
       }
       cb(null, result);
     };
 
-    proto.getBlob = function(docId, blobId, cb) {
-      var result = store.getBlob(docId, blobId);
+    this.getBlob = function(docId, blobId, cb) {
+      var result = this.store.getBlob(docId, blobId);
       if (!result) return cb(new errors.StoreError("Blob not found."));
       cb(null, result);
     };
 
-    proto.blobExists = function (docId, blobId, cb) {
-      var result = store.blobExists(docId, blobId);
+    this.blobExists = function (docId, blobId, cb) {
+      var result = this.store.blobExists(docId, blobId);
       cb(null, result);
     };
 
-    proto.deleteBlob = function(docId, blobId, cb) {
-      store.deleteBlob(docId, blobId);
+    this.deleteBlob = function(docId, blobId, cb) {
+      this.store.deleteBlob(docId, blobId);
       cb(null);
     };
 
-    proto.listBlobs = function(docId, cb) {
-      var result = store.listBlobs(docId);
+    this.listBlobs = function(docId, cb) {
+      var result = this.store.listBlobs(docId);
       cb(null, result);
     };
 
   };
+  AsyncStore.prototype = new AsyncStore.__prototype__();
 
   // Exports
   if (typeof exports !== 'undefined') {
